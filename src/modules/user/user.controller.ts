741021c3517f3c87c731e51405951cb489d4cb6d
hana <hanna@circle-vn.com>
2026-02-01 10:09:03 +0700

    update: routing and controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Query, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthService } from '../auth/auth.service';
 
@@ -7,8 +7,31 @@ export class UserController {
     constructor(private readonly userService: UserService, private readonly authService: AuthService) {
         console.log('UserController constructor');
     }
-    @Get() // -> users
+    @Get() // -> GET /users
     index() {
         return [this.userService.getUsers(), this.authService.login()];
+    }
+    
+    @Get() 
+    index2(@Query() query: any) {   // localhost:3000/users?keyword=123&category=456
+        return {
+            keyword: query.keyword,
+            category: query.category,
+        };
+    }
+
+    @Get(':id') // -> GET /users/:id    localhost:3000/users/1
+    find (@Param('id') id: string) {
+        return 'user ' + id;
+    }
+
+    @Post() // -> POST /users
+    create(@Body() body: any) {
+        return body;
+    }
+   
+    @Delete() // -> DELETE /users/:id
+    delete() {
+        return 'delete';
     }
 }
